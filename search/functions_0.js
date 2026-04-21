62231f97e9d9849fe7d680d56451e40e57ba067e
DIYLabsED <146977205+DIYLabsED@users.noreply.github.com>
2026-04-21 00:23:18 +0000

    Deploying to main from @ TarnishedEngine/tengine@abd4d04a4777799e1a4fc60909b3bc8cbd05e351 🚀

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['addsink_0',['addsink',['../classtengine_1_1util_1_1logger_1_1Logger.html#aa912cbcaaf53e6754b149bc5a0b87d89',1,'tengine::util::logger::Logger::addSink(const std::shared_ptr&lt; tengine::util::logger::ILogSink &gt; sink, const tengine::util::logger::LogFilter filter)'],['../classtengine_1_1util_1_1logger_1_1Logger.html#ae2e2945f5164112d0d8fcf5b56250efb',1,'tengine::util::logger::Logger::addSink(const std::shared_ptr&lt; tengine::util::logger::ILogSink &gt; sink)']]],
+  ['addsink_0',['addsink',['../classtengine_1_1util_1_1logger_1_1Logger.html#a01262b4889e08c84410f697708cf2b68',1,'tengine::util::logger::Logger::addSink(const std::shared_ptr&lt; tengine::util::logger::ILogSink &gt; sink, const std::shared_ptr&lt; tengine::util::logger::LogFilter &gt; filter)'],['../classtengine_1_1util_1_1logger_1_1Logger.html#ae2e2945f5164112d0d8fcf5b56250efb',1,'tengine::util::logger::Logger::addSink(const std::shared_ptr&lt; tengine::util::logger::ILogSink &gt; sink)']]],
   ['assert_5feq_1',['ASSERT_EQ',['../ParserTests_8cpp.html#a433040068db9060c366ece6b3fb4a9ac',1,'ParserTests.cpp']]]
 ];
--- search/functions_6.js
+++ search/functions_6.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['m_5fcheckinit_0',['m_checkInit',['../classtengine_1_1util_1_1logger_1_1Logger.html#a44d2692de53093147150c2a961fb6cb4',1,'tengine::util::logger::Logger']]],
-  ['m_5ffilterbylevel_1',['m_filterByLevel',['../classtengine_1_1util_1_1logger_1_1Logger.html#a85e8506c3a8f8477b8dbbb779763cd7f',1,'tengine::util::logger::Logger']]],
-  ['m_5ffilterbysender_2',['m_FilterBySender',['../classtengine_1_1util_1_1logger_1_1Logger.html#af5acbaa91d5c5e87a3aae38e048a5bd5',1,'tengine::util::logger::Logger']]],
+  ['m_5ffilterbylevel_1',['m_filterByLevel',['../classtengine_1_1util_1_1logger_1_1Logger.html#ac5175f7f67be7f421f6c6655df454804',1,'tengine::util::logger::Logger']]],
+  ['m_5ffilterbysender_2',['m_FilterBySender',['../classtengine_1_1util_1_1logger_1_1Logger.html#ad3db18e64861f1b51f4d7314a7dc0b8c',1,'tengine::util::logger::Logger']]],
   ['m_5finit_3',['m_init',['../classtengine_1_1util_1_1logger_1_1Logger.html#a22d559371b7fca54e38e6450bb40a4b3',1,'tengine::util::logger::Logger']]],
   ['m_5fsafeinternallog_4',['m_safeInternalLog',['../classtengine_1_1util_1_1logger_1_1Logger.html#a97e8d99d4d5a4b48d737e8e0d520f391',1,'tengine::util::logger::Logger']]]
 ];
